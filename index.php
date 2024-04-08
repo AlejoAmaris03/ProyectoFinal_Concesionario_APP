8dda4bdf1f23bd4fe69aaaef2f60e6511c9ae9a0
AlejoAmaris <alejoamaris2005@gmail.com>
2024-04-07 20:33:54 -0500

    Creación Index - Estilos CSS

--- index.php
+++ index.php
@@ -11,11 +11,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Estilos/estilos.css">
+    <link rel="stylesheet" href="./Estilos/estilosIndex.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Concesionario | Iniciar Sesion</title>
 </head>
 
-<body onload="limpiar()">
+<body onload="">
     <div class="contenedor">
         <div class="form">
             <div class="titulo">
@@ -23,30 +24,31 @@
             </div>
 
             <form name="form" action="" method="POST">
-                    <div class="campos">
-                        <input class="form-campos" type="text" name="usuario" id="usuario" placeholder="Nombre de Usuario">
-                    </div>
+                <div class="campos">
+                    <i class="fa-solid fa-user"></i>
+                    <input class="form-campos" type="text" name="usuario" id="usuario" placeholder="Nombre de Usuario">
+                </div>
 
-                    <div class="campos">
-                        <input class="form-campos" type="password" name="clave" id="clave" placeholder="Contraseña">
-                    </div>
+                <div class="campos">
+                    <i class="fa-solid fa-lock"></i>
+                    <input class="form-campos" type="password" name="clave" id="clave" placeholder="Contraseña">
+                </div>
 
-                    <div class="campos">
-                        <label for="tipo_usuario">Tipo de Usuario</label>
+                <div class="campos">
+                    Tipo de Usuario
 
-                        <section>
-                            <select name="estandar" id="tipo_usuario">Estándar/Normal</select>
-                            <select name="admin" id="tipo_usuario">Administrador</select>
-                        </section>
-                    </div>
+                    <select name="tipo_usuario">
+                        <option value="Estandar">Estandar</option>
+                        <option value="Administrador">Administrador</option>
+                    </select>
+                </div>
 
-                    <div class="botones">
-                        <br>
-                        <input type="hidden" name="accion" value="Acceder">
-                        <input type="button" onclick="" value="Acceder">
-                    </div>
-                </form>
-            </div>
+                <div class="botones">
+                    <br>
+                    <input type="hidden" name="accion" value="Acceder">
+                    <input type="button" onclick="" value="Acceder">
+                </div>
+            </form>
         </div>
     </div>
 </body>
