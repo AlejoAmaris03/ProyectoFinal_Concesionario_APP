f7b59c08f38b0600c1ecec4552b0eefd23f68bbc
AlejoAmaris <alejoamaris2005@gmail.com>
2024-04-07 20:01:44 -0500

    Creación Index 1.0

--- index.php
+++ index.php
@@ -1,16 +1,54 @@
+<?php
+    session_start();
+    session_unset();
+    session_destroy();
+?>
+
 <!DOCTYPE html>
 
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Concesionario | Iniciar Sesión</title>
+    <link rel="stylesheet" href="../Estilos/estilos.css">
+    <title>Concesionario | Iniciar Sesion</title>
 </head>
 
-<body>
-    
+<body onload="limpiar()">
+    <div class="contenedor">
+        <div class="form">
+            <div class="titulo">
+                <h1>Bienvenido/a</h1>
+            </div>
+
+            <form name="form" action="" method="POST">
+                    <div class="campos">
+                        <input class="form-campos" type="text" name="usuario" id="usuario" placeholder="Nombre de Usuario">
+                    </div>
+
+                    <div class="campos">
+                        <input class="form-campos" type="password" name="clave" id="clave" placeholder="Contraseña">
+                    </div>
+
+                    <div class="campos">
+                        <label for="tipo_usuario">Tipo de Usuario</label>
+
+                        <section>
+                            <select name="estandar" id="tipo_usuario">Estándar/Normal</select>
+                            <select name="admin" id="tipo_usuario">Administrador</select>
+                        </section>
+                    </div>
+
+                    <div class="botones">
+                        <br>
+                        <input type="hidden" name="accion" value="Acceder">
+                        <input type="button" onclick="" value="Acceder">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
